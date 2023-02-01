d752383c76c090830e4c1593d3d777fe70b2a29a
Robert01101101 <rmichels@clir.io>
2023-01-31 19:07:50 -0800

    Updated Prepros, added sample scan share embed from Clirio, added some basic overview text

--- development/clirioViewScanShare.php
+++ development/clirioViewScanShare.php
@@ -28,11 +28,25 @@ Partial::build('projectPageMeta', ["project" => $project]);
 
   <!----- Content / Text ----->
   <section class="sectionText">
-    <h2>Placeholder</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nisi mi, dignissim vehicula diam vel, vulputate aliquam neque. Sed ut vehicula nisl. Aenean nec purus condimentum, eleifend enim ultricies, congue diam. Phasellus magna libero, finibus ac porttitor et, sodales ut lacus. Vestibulum rutrum mi facilisis, consectetur diam in, dapibus arcu. Mauris interdum ullamcorper turpis. Donec a ullamcorper ex.</p>
-    
-    <p>Proin facilisis efficitur urna in volutpat. Integer a lectus quis purus convallis varius auctor egestas mi. Duis ultrices risus vel tortor feugiat porta. Pellentesque sem sem, scelerisque blandit suscipit ac, tincidunt non elit. Curabitur tempus volutpat condimentum. Duis ut metus nec dui mattis convallis non et felis. In nec euismod est. Ut mattis eu nisi quis laoreet. Cras nec bibendum ex. Cras libero lectus, auctor id lorem quis, fringilla vehicula odio. Donec lobortis erat ipsum, vel auctor odio tempor scelerisque. Donec vel congue arcu. Phasellus tincidunt felis sed sollicitudin pulvinar.</p>
+    <h2>Overview</h2>
+    <p>The Clirio View Scan Share feature lets users quickly share a photogrammetry scan captured with the Clirio Scan app. Pressing the share button on any of the Clirio View apps will open a pop-up to configure the share options. The user has the option to set the share to expire, password protect it, and to require that the viewer has been invited to the workspace, in which case the recipient will need to log in with their Clirio View account to view the scan.</p> 
+    <p>Once the user is satisfied with their selected options, they can generate the link, and either copy it to clipboard or use the share function to trigger the share options native to each platform. For instance, on iOS this will open the sharing options with the option to pick from compatible apps such as messengers, while on Windows Desktop the Mail app will open.</p>
+    <p>There is also an option for users to instead embed the link, in which case the user can configure their desired with and height, and the app will format an embed iframe element code snippet accordingly for the user to copy and embed on their website.</p>
   </section>
+
+
+  <!----- Content / Embed 3D Scan ----->
+  <iframe src="https://clirioview-viw-prd.azurewebsites.net/guest/MEVdEANk9_Zhr6tlm4ibd1Vn" style="width: 100%" class="clirioScanShareEmbed"></iframe>
+
+
+  <!----- Content / Text ----->
+  <section class="sectionText">
+    <h2>Placeholder</h2>
+    <p>Sed dapibus nisi sem, vel ornare ex sollicitudin vitae. Vestibulum magna ante, lacinia eget maximus sed, sagittis gravida nunc. Maecenas tempus sem ac odio pulvinar, ut posuere nibh efficitur. Nulla malesuada rutrum auctor. Sed fermentum eleifend risus, sed pulvinar erat. Suspendisse non augue eu erat consectetur tincidunt et vel nisi. Aenean et dui nec massa pellentesque pellentesque eget at velit. Vivamus eget metus ut ex aliquet suscipit ac a purus</p>
+
+    <p>Donec vel dignissim tortor, id porttitor metus. Donec sit amet elementum mauris. Integer vehicula vestibulum lacus pulvinar mattis. Nunc vitae ligula vel massa posuere vulputate id ac velit. Suspendisse potenti. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur tortor odio, finibus eu gravida ac, vehicula id leo. Donec molestie ante sit amet libero elementum efficitur. Cras lobortis ut ligula ac scelerisque. Ut pretium mi nec porta pharetra.</p>
+  </section>
+
 
   <!----- Content / Media ----->
   <section class="sectionMedia">
@@ -54,15 +68,8 @@ Partial::build('projectPageMeta', ["project" => $project]);
       <figcaption>Caption</figcaption>
     </figure>
   </section>
-  
 
-  <!----- Content / Text ----->
-  <section class="sectionText">
-    <h2>Placeholder</h2>
-    <p>Sed dapibus nisi sem, vel ornare ex sollicitudin vitae. Vestibulum magna ante, lacinia eget maximus sed, sagittis gravida nunc. Maecenas tempus sem ac odio pulvinar, ut posuere nibh efficitur. Nulla malesuada rutrum auctor. Sed fermentum eleifend risus, sed pulvinar erat. Suspendisse non augue eu erat consectetur tincidunt et vel nisi. Aenean et dui nec massa pellentesque pellentesque eget at velit. Vivamus eget metus ut ex aliquet suscipit ac a purus</p>
 
-    <p>Donec vel dignissim tortor, id porttitor metus. Donec sit amet elementum mauris. Integer vehicula vestibulum lacus pulvinar mattis. Nunc vitae ligula vel massa posuere vulputate id ac velit. Suspendisse potenti. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur tortor odio, finibus eu gravida ac, vehicula id leo. Donec molestie ante sit amet libero elementum efficitur. Cras lobortis ut ligula ac scelerisque. Ut pretium mi nec porta pharetra.</p>
-  </section>
 </div>
 
 <?php
