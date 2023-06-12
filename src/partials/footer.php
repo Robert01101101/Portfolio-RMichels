de063eb9e1a2bb6b85a857fce7ba3522c2453af2
Robert01101101 <rmichels@clir.io>
2023-06-12 13:55:36 -0700

    Updated footer and header to remove deprecated code and update external links to current profiles

--- src/partials/footer.php
+++ src/partials/footer.php
@@ -17,7 +17,7 @@
               </div>
               <small class="js-emaillink">hi@rmichels.com</small> 
               <a href="https://linkedin.com/in/robert-michels" target="_blank" title="LinkedIn"><li><i class="fab fa-linkedin-in fa-2x"></i></li></a>
-              <a href="https://github.com/robert-michels" target="_blank" title="GitHub"><li><i class="fab fa-github fa-2x"></i></li></a>
+              <a href="https://github.com/Robert01101101" target="_blank" title="GitHub"><li><i class="fab fa-github fa-2x"></i></li></a>
               <a href="https://play.google.com/store/apps/developer?id=Studio+RM" target="_blank" title="Google Play"><li><i class="fab fa-google-play fa-2x"></i></li></a>
               <a href="https://rmichels.itch.io/" target="_blank" ><li><i class="fab fa-itch-io fa-2x"title="Itch.io"></i></li></a>
             </ul>
@@ -36,24 +36,6 @@
   <div id="copyConfirm" class="hidden"><small>Copied to Clipboard!</small></div>
 
   <?php if(!isset($_COOKIE["returningVisitor"])) : ?>
-
-    <!--<div id="tmpVisitorPopUp">
-      <div>
-        <h2>Hello There!</h2>
-        <p>This website is in development &#38; is expected to be finished by Mid-February 2020.</p>
-        <p>If you are a stranger who just happened to stumble upon this, feel free to explore, but be warned - things aren't quite done yet.</p>
-        <p>If you would like to give me feedback, that would be much appreciated!</p>
-        <div>
-          <a href="https://forms.gle/kWRg97XYz8k5jd8u7" target="_blank" onclick="closePopUp()">Open survey in another tab &#8594;</a>
-          <button onclick="closePopUp()">Close &#38; explore site &#8594;</button>
-        </div>   
-      </div>
-    </div>
-    <script>
-    function closePopUp() {
-      document.getElementById("tmpVisitorPopUp").style.display = "none";
-    }
-    </script>-->
 
   <?php endif; ?>
 
@@ -100,8 +82,6 @@
   <script src="<?php echo $GLOBALS['d'];?>js/three/examples/js/loaders/GLTFLoader.js"></script>
   <script src="<?php echo $GLOBALS['d'];?>js/tools.js?v=1.0.0"></script>
   <script src="<?php echo $GLOBALS['d'];?>js/lqip.js?v=1.0.0"></script>
-  <!--<script src="<?php echo $GLOBALS['d'];?>three/myThreeScript.js"></script>-->
-  <!--<script src="<?php echo $GLOBALS['d'];?>three/skull.js"></script>-->
 
   <script src="<?php echo $GLOBALS['d'];?>js/three/waves.js?v=1.0.0"></script>
   
