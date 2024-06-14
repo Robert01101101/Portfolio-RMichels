432e54b658bb9d37e2bdd4bf7d271d5c43412b69
Robert01101101 <hi@rmichels.com>
2024-06-14 00:13:44 -0700

    disabled section Text bg color (appears on some screens)

--- src/partials/header.php
+++ src/partials/header.php
@@ -45,7 +45,7 @@ setcookie("returningVisitor", "true");
 
 
   <link rel="stylesheet" href="<?php echo $GLOBALS['d'];?>css/normalize.css">
-  <link rel="stylesheet" href="<?php echo $GLOBALS['d'];?>css/main.css?v=1.0.8">
+  <link rel="stylesheet" href="<?php echo $GLOBALS['d'];?>css/main.css?v=1.0.9">
 
   <!-- Load Font -->
   <!-- Load Icons -->
