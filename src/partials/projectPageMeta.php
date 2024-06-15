192faaac5200ddb7d03810dccf9c4a906842936d
Robert01101101 <hi@rmichels.com>
2024-06-14 21:24:13 -0700

    Translated Clirio Cloud page

--- src/partials/projectPageMeta.php
+++ src/partials/projectPageMeta.php
@@ -4,15 +4,15 @@
 
 <div id="projMeta"> <!--___________ Proj Meta ____________-->
   <div class="projMetaItem">
-    <h2>Type</h2>
+    <h2><?php echo _("Type"); ?></h2>
     <p><?php echo $GLOBALS['english'] ? $project->getType() : $project->getTypeDe() ?></p>
   </div>
   <div class="projMetaItem">
-    <h2>Year</h2>
+    <h2><?php echo _("Year"); ?></h2>
     <p><?php echo $project->getYear() ?></p>
   </div>
   <div class="projMetaItem">
-    <h2>Skills Applied</h2>
+    <h2><?php echo _("Skills Applied"); ?></h2>
     <ul>
       <?php 
 
@@ -44,7 +44,7 @@
       ?>
     </ul>
     <?php else: ?>
-      <h2>Company</h2>
+      <h2><?php echo _("Company"); ?></h2>
       <a href="https://clir.io/" target="_blank"><?php echo $project->getCompany() ?></a>
     <?php endif; ?>
 
