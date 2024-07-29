9e32a05090050d6cca989c1b00d6c955b4abdefb
Robert01101101 <hi@rmichels.com>
2024-07-28 19:32:47 -0700

    tourguide sites added

---
+++ tourguideDomain/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tourguide</title>
+  <link rel="stylesheet" href="css/main.css">
+  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400&family=Vollkorn:wght@400&display=swap" rel="stylesheet">
+</head>
+<body>
+<h1>Tourguide</h1>
+<p>Tourguide's web presence is still under development.</p>
+<ul>
+    <li><a href="communityGuidelines.html">Community Guidelines</a></li>
+    <li><a href="termsOfService.html">Terms of Service</a></li>
+    <li><a href="privacyPolicy.html">Privacy Policy</a></li>
+</ul>
+</body>
+</html>
