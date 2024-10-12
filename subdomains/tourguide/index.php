5a395c2fd00d5ca8a8c42285ed577aa06896db11
Robert01101101 <hi@rmichels.com>
2024-10-11 21:05:50 -0700

    more wip for tourguide landing page

--- subdomains/tourguide/index.php
+++ subdomains/tourguide/index.php
@@ -8,19 +8,33 @@
   <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400&family=Vollkorn:wght@400&display=swap" rel="stylesheet">
 </head>
 <body>
-<h1>Tourguide</h1>
-<p>Tourguide is available for:</p>
-<ul>
-    <li>Web: &nbsp;&nbsp;&nbsp;<a href="https://tourguide-firebase.web.app/">tourguide-firebase.web.app</a></li>
-    <li>Android: &nbsp;&nbsp;&nbsp;<a href="https://play.google.com/store/apps/details?id=com.robertmichelsdigitalmedia.tourguideapp">Google Play Store</a></li>
-</ul>
-<p>Helpful Links:</p>
-<ul>
-    <li><a href="communityGuidelines.html">Community Guidelines</a></li>
-    <li><a href="termsOfService.html">Terms of Service</a></li>
-    <li><a href="privacyPolicy.html">Privacy Policy</a></li>
-    <li><a href="accountDeletion.html">Account Deletion Instructions</a></li>
-    <li><a href="https://rmichels.com/tourguide">Read More about Tourguide</a></li>
-</ul>
+  <img src="img/stephan-valentin-rwFQP0L2Xl4-unsplash-sml-crp.png" alt="Unsplash - Stéphan Valentin - city-buildings-near-sea-under-white-clouds-during-daytime-rwFQP0L2Xl4" class="img-fullscreen">
+  <main>
+    <section id='landing' class="row">
+      <div class="column hero-wrapper">
+        <h1>Tourguide</h1>
+        <div class="hero">Discover tours around the world, plan your itinerary and learn more about your destinations.</div>
+        <div class="hero-sub">Join a community of travelers and discoverers, plan your sightseeing trip, and give your insights to other travelers.</div>
+        <div class="row gap">
+          <div class="hero-button"><a href="https://play.google.com/store/apps/details?id=com.robertmichelsdigitalmedia.tourguideapp">Android App</a></div>
+          <div class="hero-button"><a href="https://tourguide-firebase.web.app/">Web App</a></div>
+        </div>
+      </div>
+      <div>
+        <img src="img/mockup.png" alt="Mockup">
+      </div>
+    </section>
+
+    <section id='documents'>
+      <p>Helpful Links:</p>
+      <ul>
+          <li><a href="communityGuidelines.html">Community Guidelines</a></li>
+          <li><a href="termsOfService.html">Terms of Service</a></li>
+          <li><a href="privacyPolicy.html">Privacy Policy</a></li>
+          <li><a href="accountDeletion.html">Account Deletion Instructions</a></li>
+          <li><a href="https://rmichels.com/tourguide">Read More about Tourguide</a></li>
+      </ul>
+    </section>
+  </main>
 </body>
 </html>
