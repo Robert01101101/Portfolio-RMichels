bb9dd8877960a75eae7ae553a8b61210c98a8698
Robert01101101 <hi@rmichels.com>
2024-10-11 21:59:02 -0700

    finished v1 of tourguide landing page

--- subdomains/tourguide/index.php
+++ subdomains/tourguide/index.php
@@ -10,23 +10,25 @@
 <body>
   <img src="img/stephan-valentin-rwFQP0L2Xl4-unsplash-sml-crp.png" alt="Unsplash - Stéphan Valentin - city-buildings-near-sea-under-white-clouds-during-daytime-rwFQP0L2Xl4" class="img-fullscreen">
   <main>
-    <section id='landing' class="row">
-      <div class="column hero-wrapper">
-        <h1>Tourguide</h1>
-        <div class="hero">Discover tours around the world, plan your itinerary and learn more about your destinations.</div>
-        <div class="hero-sub">Join a community of travelers and discoverers, plan your sightseeing trip, and give your insights to other travelers.</div>
-        <div class="row gap">
-          <div class="hero-button"><a href="https://play.google.com/store/apps/details?id=com.robertmichelsdigitalmedia.tourguideapp">Android App</a></div>
-          <div class="hero-button"><a href="https://tourguide-firebase.web.app/">Web App</a></div>
+    <section id='landing'>
+      <div class="row">
+        <div class="column hero-wrapper">
+          <h1>Tourguide</h1>
+          <div class="hero">Discover tours around the world, plan your itinerary and learn more about your destinations.</div>
+          <div class="hero-sub">Join a community of travelers and discoverers, plan your sightseeing trip, and give your insights to other travelers.</div>
+          <div class="row gap">
+            <a href="https://play.google.com/store/apps/details?id=com.robertmichelsdigitalmedia.tourguideapp"  class="hero-button">Android App</a>
+            <a href="https://tourguide-firebase.web.app/" class="hero-button">Web App</a>
+          </div>
         </div>
-      </div>
-      <div>
-        <img src="img/mockup.png" alt="Mockup">
+        <div class="img-wrapper">
+          <img src="img/mockup.png" alt="Mockup">
+        </div>
       </div>
     </section>
 
-    <section id='documents'>
-      <p>Helpful Links:</p>
+    <section id='documents' class="glass-container">
+      <p>Helpful Links</p>
       <ul>
           <li><a href="communityGuidelines.html">Community Guidelines</a></li>
           <li><a href="termsOfService.html">Terms of Service</a></li>
